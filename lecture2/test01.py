6e446707aad1ad77b8b34d4ee6b3614fc7cb7be1
Phiraphat29 <s6652410029@sau.ac.th>
2023-08-22 09:48:08 +0700

    code V.1: Create 4 files.

---
+++ lecture2/test01.py
@@ -0,0 +1,17 @@
+print("Hello, \
+World.")
+print("Hi, World.")
+print("Hello, World.")
+print("Hum, World.")
+print("123456")
+
+print('''
+      DTI
+      ^o^
+      SAU
+      ''')
+
+print("""
+      ID : 6652410029
+      NAME : Phiraphat
+      """)
